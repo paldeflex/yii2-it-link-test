925415a1e5aa8569062db40133784285f85db4c6
paldeflex <paldeflex@gmail.com>
2026-03-11 12:24:16 +0500

    Установка и настройка Yii2 basic проекта с Docker

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=postgres;dbname=cars_db',
+    'username' => 'cars_user',
+    'password' => 'secret',
+    'charset' => 'utf8',
+];
